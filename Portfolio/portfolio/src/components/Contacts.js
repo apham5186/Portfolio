f72f151dc36633fc6f0692fc209cd9c798cddac7
apham5186 <apham5186@gmail.com>
2023-10-30 13:07:59 -0400

    Added contact section functionality

--- Portfolio/portfolio/src/components/Contacts.js
+++ Portfolio/portfolio/src/components/Contacts.js
@@ -1,15 +1,29 @@
 
-import React from "react";
+import React, {useRef} from "react";
 import './Contacts.css';
+import emailjs from '@emailjs/browser';
 const Contacts = ()=>{
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_wvoyjxb', 'template_9alshyg', form.current, '8mhmg3y4rMDD8AVWJ')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+          alert('Email Sent!');
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
       <section id='contactsPg'>
          <div className="contactBox">
           <h1 className="contactTitle">Connect!</h1>
           <span className="contactDesc"> Please fill out the form below to discuss any work opportunities.</span>
-          <form className="contactForm">
-            <input type='text'className="name"placeholder="Your Name"/>
-            <input type="email" className="email" placeholder="Your Email"/>
+          <form className="contactForm" ref={form} onSubmit={sendEmail}>
+            <input type='text'className="name"placeholder="Your Name" name='your_name' />
+            <input type="email" className="email" placeholder="Your Email" name='your_email' />
             <textarea className ='message' cols="30" rows="10" placeholder="Say something nice!"></textarea>
             <button type='submit' value='Send'className="sendBtn">SEND!</button>
           </form>
--- Portfolio/portfolio/src/components/Navbar.js
+++ Portfolio/portfolio/src/components/Navbar.js
@@ -6,12 +6,13 @@ const Navbar = () => {
         <nav className= "navbar">
             <img src="PhamALogo.png" alt="" className = 'logo'/>
             <div className="desktopMenu">
-                <Link activeClass= 'active' to = 'intro' className="desktopMenuListItem">Home</Link>
-                <Link activeClass= 'active' to = 'about' className="desktopMenuListItem">About</Link>
-                <Link activeClass = 'active' to='skills' className="desktopMenuListItem">Skills</Link>
-                <Link activeClass = 'active' to = 'projects' className="desktopMenuListItem">Projects</Link>
+                <Link activeClass= 'active' to = 'intro' spy = {true} smooth = {true} className="desktopMenuListItem">Home</Link>
+                <Link activeClass= 'active' to = 'about' spy = {true} smooth = {true} offset = {-100} className="desktopMenuListItem">About</Link>
+                <Link activeClass = 'active' to= 'skills' spy = {true} smooth = {true} offset = {-20} className="desktopMenuListItem">Skills</Link>
+                <Link activeClass = 'active' to = 'projects' spy = {true} smooth = {true} offset = {-50} className="desktopMenuListItem">Projects</Link>
             </div>
-            <button className="desktopMenuBtn">
+            <button className="desktopMenuBtn" onClick={() => {
+                document.getElementById('contactsPg').scrollIntoView({behavior:'smooth'});}}>
                 <img src="contact.png" alt="" className="desktopMenuImg" />Contact Me</button>
         </nav>
     )
